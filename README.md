51b43c9d233fbd78cd91c71468ae7fa48ae47237
cha <chyb627@naver.com>
2022-08-18 00:28:30 +0900

    데이터베이스와 연결

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ npm install typescript ts-node @types/node @types/express @types/morgan --save-d
 npx tsc --init
 
 -tsconfig.js : TypeScript로 짜여진 코드를 JavaScript로 컴파일하는 옵션을 설정하는 파일. TypeScript 컴파일은 tsc라는 명령어를 사용.
+
+
+## 추가 모듈 설치
+
+npm install pg typeorm reflect-metadata --save
+
+- pg : postgreSQL 데이터베이스와 인터페이스하기 위한 nodeJS 모듈 모음
+- typeorm : TypeScript 및 JavaScript 와 함께 사용할 수 있는 NodeJS에서 실행되는 ORM.
+
+
+## typeorm 설정
+npx typeorm init
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import { AppDataSource } from "./data-source"
 
 const app = express();
 
@@ -14,4 +15,8 @@ let port = 4000;
 // app.listen의 포트로 접속하면 해당 블록의 코드를 실행합니다.
 app.listen(port, async () => {
     console.log(`server running at http://localhost:${port}`);
+
+    AppDataSource.initialize().then(() => {
+        console.log("database initialized")
+    }).catch(error => console.log(error))
 })
