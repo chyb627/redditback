5e86e697ef55a0b7906ead92ef2067451d3926a5
cha <chyb627@naver.com>
2022-08-28 19:03:17 +0900

    엔티티 정의

--- README.md
+++ README.md
@@ -25,3 +25,27 @@ npm install pg typeorm reflect-metadata --save
 
 ## typeorm 설정
 npx typeorm init
+
+- typeorm : typeorm은 nodejs에서 실행되고 TypeScript로 작성된 객체 관계형  매퍼 라이브러리. typeorm은 MySQL, PostgreSQL, MariaDB, SQLite등 여러 데이터베이스를 지원함.
+
+- ORM (Object Relational Mapping) : 객체와 관계형 데이터베이스의 데이터를 자동으로 변형 및 연결하는 작업. ORM을 이용한 개발은 객체와 데이터베이스의 변형에 유연하게 사용할 수 있다.
+
+
+## 엔티티 생성을 위해 필요한 모듈 설치
+
+npm install bcryptjs class-transformer class-validator --save
+npm install @types/bcryptjs --save-dev
+
+- bcryptjs : 비밀번호를 암호화해서 데이터베이스에 저장할 수 있게 한다.
+- class-transformer : 일반 개체를 클래스 일부 인스턴스로 또는 그 반대로 변환
+- class-validator : 데코레이터를 이용해서 요청에서오는 오브젝트의 프로퍼티를 검증하는 라이브러리.
+
+
+## One To Many, Many To One
+
+- 유저와 게시물 데이터의 관계 형성 (관계를 형성하기 위해서는 엔티티에 서로간의 필드를 넣어줘야 합니다.)
+              - 게시글1
+user          - 게시글2
+              - 게시글3
+
+OneToMany      ManyToOne
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,18 +1,49 @@
-// import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { IsEmail, Length } from "class-validator";
+import { Entity, PrimaryGeneratedColumn, Column, Index, In, OneToMany, BeforeInsert } from "typeorm"
+import bcrypt from 'bcryptjs';
+import Post from "./Post";
+import Vote from "./Vote";
+import BaseEntity from './Entity';
 
-// @Entity()
-// export class User {
+@Entity("users")
+// Entity() 데코레이터 클래스는 User클래스가 엔티티임을 나타내는데 사용된다.
+// CREATE TABLE user 부분이다
 
-//     @PrimaryGeneratedColumn()
-//     id: number
+export class User extends BaseEntity {
 
-//     @Column()
-//     firstName: string
+    // Column() 데코레이터 클래스는 User엔티티의 email 및 username과 같은 다른 열을 나타내는데 사용된다.
+    // Index()는 데이터베이스 인덱스를 생성한다. 엔티티 속성 또는 엔티티에 사용할 수 있다. 엔티티에 사용될 때 복합열로 인덱스를 생성할 수 있다.
 
-//     @Column()
-//     lastName: string
+    // 데이터배에스에 인덱스 생성 이유는? 
+    // 책에서 목차가 특정 주제를 더 빨리찾을 수 있듯이 그러한 역할을 한다. 
+    // 테이블 쿼리 속도를 올려준다. 특정 컬럼값을 가지고 열이나 값을 빠르게 찾을 수 있게 해준다.
+    // 어떠한 정보를 찾을 때 처음부터 모든 데이터를 조회x . 데이터 파일 중간에서 검색위치를 빠르게 잡을 수 있다.
+    // 데이터 양이 많고 변경보다 검색이 빈번한 경우 인덱싱을 하면 좋다.
 
-//     @Column()
-//     age: number
+    @Index()
+    @IsEmail(undefined, { message: "이메일 주소가 잘못되었습니다." })
+    @Length(1, 255, { message: "이메일 주소는 비워둘 수 없습니다." })
+    @Column({ unique: true })
+    email: string;
 
-// }
+    @Index()
+    @Length(3, 32, { message: "사용자 이름은 3자 이상이어야 합니다." })
+    @Column({ unique: true })
+    username: string
+
+    @Column()
+    @Length(6, 255, { message: '비밀번호는 6자리 이상이어야 합니다.' })
+    password: string;
+
+    @OneToMany(() => Post, (post) => post.user)
+    posts: Post[]
+
+    @OneToMany(() => Vote, (vote) => vote.user)
+    votes: Vote[]
+
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await bcrypt.hash(this.password, 6)
+    }
+
+}
